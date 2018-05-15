34f4b7af3451bf0d7c6c541008ffeac2f80b92f9
dragosdumitru04 <dragosdumitru04@gmail.com>
2018-05-15 22:43:46 +0300

    Homework01

--- Module01Week01/Homework01/Program.cs
+++ Module01Week01/Homework01/Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Homework01
+namespace PentaStagiuWeek1
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Random rng = new Random();
+            int target = rng.Next(0, 100);
+
+            Console.Write("Input a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            while (number != target)
+            {
+                if (number < target)
+                {
+                    Console.WriteLine("The number was too small");
+                }
+                else if (number > target)
+                {
+                    Console.WriteLine("The number was too big");
+                }
+
+                Console.Write("Input a number: ");
+                number = Convert.ToInt32(Console.ReadLine());
+            }
+
+            Console.WriteLine("Congratulations! You guessed the number!");
         }
     }
 }
