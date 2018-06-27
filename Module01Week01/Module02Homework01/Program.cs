7d82252da1e0b566d99e85b1a489223612eb3670
dragosdumitru04 <dragosdumitru04@gmail.com>
2018-06-27 14:47:13 +0300

    Module02Homework01

---
+++ Module01Week01/Module02Homework01/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Module02Homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = File.ReadAllLines("people.txt").ToList();
+
+            Console.WriteLine("Add a name: ");
+            string name1 = Console.ReadLine();
+            AddName(name1, list);
+
+            Console.WriteLine("Add another name: ");
+            string name2 = Console.ReadLine();
+            AddName(name2, list);
+
+            list.Remove("Iulian");
+
+            try
+            {
+                if (!File.Exists("output.txt"))
+                {
+                    throw new FileNotFoundException();
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                var file = File.Create("output.txt");
+                file.Close();
+            }
+
+            File.WriteAllLines("output.txt", list);
+        }
+
+        private static void AddName(string name, List<string> list)
+        {
+            if (name.All(Char.IsLetter))
+            {
+                list.Add(name);
+            }
+            else
+            {
+                Console.WriteLine("The name is invalid");
+            }
+        }
+    }
+}
--- Module01Week01/Module02Homework01/Properties/AssemblyInfo.cs
+++ Module01Week01/Module02Homework01/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("19cc676d-cddd-4d8c-8d98-5d099db99fce")]
+[assembly: Guid("ff34dc96-194f-490d-80ca-f6339dfe5932")]
 
 // Version information for an assembly consists of the following four values:
 //
